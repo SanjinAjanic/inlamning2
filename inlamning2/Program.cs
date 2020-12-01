658b032a5076168e1aa7fcd1084ccfdbf1abc51b
Sanjin Ajanic <sanjin.ajanic@hotmail.com>
2020-12-01 16:35:19 +0100

    Add project files.

---
+++ inlamning2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace inlamning2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pWord;
+
+            do
+            {
+                Console.Write("Enter Password:");
+                pWord = Console.ReadLine();
+
+                if (pWord != "bästkusten")
+                {
+                    Console.WriteLine("Wrong password try again! ");
+                }
+               
+
+            } while (pWord != "bästkusten");
+
+            
+            
+
+            Console.WriteLine("1) Välj en anvädare ");
+            Console.WriteLine("2) Ta bort en användare");
+            Console.WriteLine("3) Stäng av programmet");
+
+        }
+    }
+}
