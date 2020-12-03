70f124983a130fecaeac6d2383f300ef2bfafcf7
Sanjin Ajanic <sanjin.ajanic@hotmail.com>
2020-12-03 14:21:30 +0100

    skapat en konstruktor, skapat meny och en metod för inloggning + felhantering av menyn

---
+++ inlamning2/Class1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inlamning2
+{
+    class Members 
+    {
+        //Fält
+        private string name;
+        private string hobby;
+        private string favFood;
+        private string favColor;
+        private string motivation;
+        private string homTown;
+        private string birthPlace;
+        private int age;
+        private int length;
+        private int siblings;
+
+
+
+        /*
+        //Property
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Hobby
+        {
+            get
+            {
+                return hobby;
+            }
+            set
+            {
+                hobby = value;
+            }
+
+        }
+        */
+
+        
+        
+   
+        //Konstruktor
+        public Members(string name, string hobby, string favFood, string favColor, string motivation, string homTown, string birthPlace, int age, int length, int siblings)
+        {
+            this.name = name;
+            this.hobby = hobby;
+            this.favFood = favFood;
+            this.favColor = favColor;
+            this.motivation = motivation;
+            this.homTown = homTown;
+            this.birthPlace = birthPlace;
+            this.age = age;
+            this.length = length;
+            this.siblings = siblings;
+
+
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public string Hobby
+        {
+            get
+            {
+                return hobby;
+            }
+        }
+
+    }
+}
+
+
--- inlamning2/Program.cs
+++ inlamning2/Program.cs
@@ -1,10 +1,72 @@
 ﻿using System;
+using System.Threading;
 
 namespace inlamning2
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            string input = "";
+            bool isNum = false;
+            int userInPut = 0;
+            
+            
+            MainMenu();
+
+            Members Jerry = new Members("Jerry", "Gaming", "favFood", "favColor", "motivation", "homTwon", "birthPlace", 19,181,1 );
+         
+           
+            Members David = new Members("David ", "hobby ", "favFood", "favColor", "motivation", "homTwon", "birthPlace", 19, 181, 1);           
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1) Välj en anvädare ");
+                Console.WriteLine("2) Ta bort en användare");
+                Console.WriteLine("3) Stäng av programmet");
+
+
+                input = Console.ReadLine();
+                isNum = int.TryParse(input, out userInPut);
+            } while (isNum == false);
+            switch  (userInPut)
+          
+            {
+                //Skriver ut namnen
+                case 1:
+                    do
+                    {
+                        Console.Clear();
+                        Console.WriteLine("1. " + Jerry.Name);
+                        Console.WriteLine("2. " + David.Name);
+                        input = Console.ReadLine();
+                        isNum = int.TryParse(input, out userInPut);
+
+                    } while (isNum == false);
+                   
+                    //Skriver ut all indormation om användaren
+                    switch (userInPut)
+                    {
+                        
+                        case 1:
+                            Console.WriteLine($"{Jerry.Name} {Jerry.Hobby}");
+                            break;
+                        default:
+                            break;
+                    }
+
+                    break;
+                default:
+                    break;
+ 
+
+
+
+            }
+            
+
+        }
+        static void MainMenu()
         {
             string pWord;
 
@@ -16,19 +78,12 @@ namespace inlamning2
                 if (pWord != "bästkusten")
                 {
                     Console.WriteLine("Wrong password try again! ");
+                    Thread.Sleep(5000);
+                    Console.Clear();
                 }
-               
+
 
             } while (pWord != "bästkusten");
-
-            
-            
-
-            Console.WriteLine("1) Välj en anvädare ");
-            Console.WriteLine("2) Ta bort en användare");
-            Console.WriteLine("3) Stäng av programmet");
-            
-
         }
     }
 }
